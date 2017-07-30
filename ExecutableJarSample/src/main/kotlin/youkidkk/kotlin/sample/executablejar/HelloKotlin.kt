21e057dbc276654ac0be58710f34781304e99233
youkidkk <youkidkk80@gmail.com>
2017-07-30 18:17:00 +0900

    Initial commit

---
+++ ExecutableJarSample/src/main/kotlin/youkidkk/kotlin/sample/executablejar/HelloKotlin.kt
@@ -0,0 +1,6 @@
+package youkidkk.kotlin.sample.executablejar
+
+fun main(args: Array<String>) {
+    println("Hello Kotlin.")
+    println("This is ExecutableJar!!!")
+}
---
+++ JarSample/src/main/kotlin/youkidkk/kotlin/sample/jar/HelloKotlin.kt
@@ -0,0 +1,6 @@
+package youkidkk.kotlin.sample.jar
+
+fun main(args: Array<String>) {
+    println("Hello Kotlin.")
+    println("This is Jar!!!")
+}
